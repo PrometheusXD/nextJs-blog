0970a78f89af62f48e68084082dcb693da2cc227
Matheus <matheus.rodrigues2010@hotmail.com>
2023-06-10 20:18:35 -0300

    Initial commit from Create Next App

---
+++ src/components/CloneMe/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { CloneMe, CloneMeProps } from '.';
+
+export default {
+  title: 'CloneMe',
+  component: CloneMe,
+} as Meta;
+
+export const Template: StoryFn<CloneMeProps> = (args) => <CloneMe {...args} />;
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  url: 'http://localhost:1337/api/pages/?&populate=deep&',
+  siteName: 'Otávio Miranda',
+  defaultSlug: 'landing-page',
+};
