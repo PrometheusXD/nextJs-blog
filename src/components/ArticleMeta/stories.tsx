b9f8bd2c7a6c8fd6fab1f64355f86ff0a76bd280
Matheus <matheus.rodrigues2010@hotmail.com>
2023-06-11 08:21:52 -0300

    created AticleMeta component

---
+++ src/components/ArticleMeta/stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ArticleMeta, ArticleMetaProps } from '.';
+
+import mock from './mock';
+
+export default {
+  title: 'ArticleMeta',
+  component: ArticleMeta,
+  args: mock,
+  argTypes: {
+    createdAt: {
+      control: {
+        type: 'date',
+      },
+    },
+
+    author: { type: 'symbol' },
+    categories: { type: 'symbol' },
+  },
+} as Meta<ArticleMetaProps>;
+
+export const Template: StoryFn<ArticleMetaProps> = (args) => {
+  return (
+    <div>
+      <ArticleMeta {...args} />
+    </div>
+  );
+};
---
+++ src/components/ArticleMeta/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { ArticleMeta, ArticleMetaProps } from '.';
+
+import mock from './mock';
+
+const props: ArticleMetaProps = mock;
+
+describe('<ArticleMeta />', () => {
+  it('should render author and category links', () => {
+    const { container } = renderTheme(<ArticleMeta {...props} />);
+
+    expect(
+      screen.getByRole('link', { name: 'Otávio Miranda' }),
+    ).toHaveAttribute('href', '/author/otavio-miranda');
+    expect(screen.getByRole('link', { name: 'Tech' })).toHaveAttribute(
+      'href',
+      '/category/tech',
+    );
+    expect(screen.getByRole('link', { name: 'JS' })).toHaveAttribute(
+      'href',
+      '/category/javascript',
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should format date', () => {
+    renderTheme(<ArticleMeta {...props} />);
+
+    expect(screen.getByText('2 de mar. de 2021')).toHaveAttribute(
+      'datetime',
+      props.createdAt,
+    );
+  });
+});
