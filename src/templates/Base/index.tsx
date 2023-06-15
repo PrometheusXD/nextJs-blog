d23dddcf116c297aaa4f20a34a26841f5a57e4e1
Matheus <matheus.rodrigues2010@hotmail.com>
2023-06-14 23:13:41 -0300

    Base template was created

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,39 @@
+import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
+import { Header } from '../../components/Header';
+import { Menu } from '../../components/Menu';
+import { SettingsStrapi } from '../../shared-types/settings-strapi';
+import * as Styled from './styles';
+
+export type BaseProps = {
+  settings: SettingsStrapi;
+  children: React.ReactNode;
+};
+
+export const Base = ({ settings, children }: BaseProps) => {
+  return (
+    <Styled.Wrapper>
+      <Menu
+        links={settings.menuLink}
+        blogName={settings.blogName}
+        logo={settings.logo.url}
+      />
+
+      <Styled.HeaderContainer>
+        <Header
+          blogName={settings.blogName}
+          blogDescription={settings.blogDescription}
+          logo={settings.logo.url}
+        />
+      </Styled.HeaderContainer>
+
+      <Styled.ContentContainer>{children}</Styled.ContentContainer>
+
+      <Styled.FooterContainer>
+        <Footer footerHtml={settings.text} />
+      </Styled.FooterContainer>
+
+      <GoTop />
+    </Styled.Wrapper>
+  );
+};
