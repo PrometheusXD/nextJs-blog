63023086917d2d40e036e128660d61053a372b4f
Matheus <matheus.rodrigues2010@hotmail.com>
2023-06-15 05:12:08 -0300

    Searching graphql data with graphql-request

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/contexts/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/graphql/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/components/Comments/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/shared-types/**/*.{js,jsx,ts,tsx}',
-    '!<rootDir>/src/shared-typed/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
@@ -18,7 +13,10 @@ module.exports = {
   ],
 
   testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
 
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
